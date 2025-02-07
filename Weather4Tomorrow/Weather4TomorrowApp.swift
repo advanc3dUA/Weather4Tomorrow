27835b458d9afe6694654b140b9d5713f1ffbbd2
Yuriy Gudimov <fash1k@gmail.com>
2025-02-07 15:16:42 +0100

    Initial commit

---
+++ Weather4Tomorrow/Weather4TomorrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather4TomorrowApp.swift
+//  Weather4Tomorrow
+//
+//  Created by Yuriy Gudimov on 07.02.25.
+//
+
+import SwiftUI
+
+@main
+struct Weather4TomorrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
